27b10bbb8afd0c6e5fab6b8fd7fd947776c03c44
m.kadochnikov <m.kadochnikov@goodt.me>
2020-02-01 20:27:13 +0500

    исправлялЗаКриворукими!

--- web_project/accounts/urls.py
+++ web_project/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.contrib.auth.views import LoginView, LogoutView
+from registration.views import signup
 
 app_name = 'registrations'
 urlpatterns = [
     url('login', LoginView.as_view(), name='login'),
     url('logout', LogoutView.as_view(), name='logout'),
+    url('signup/', signup, name='sigup'),
   
 ]
--- web_project/main/urls.py
+++ web_project/main/urls.py
@@ -3,6 +3,7 @@ from . import views
 import profac.views
 import cheque.views
 from django.conf.urls import url
+from registration.views import signup
 from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('', views.index, name='index'),
@@ -10,4 +11,5 @@ urlpatterns = [
     path('profile/', profac.views.index, name='profile'),
     path('cheque/', cheque.views.post_new, name='cheque'),
     url('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    url('signup/', signup, name='signup'),
 ]
