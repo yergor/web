ca72ade0940243f809ee0231b82f369c0a8d68fe
g.ericyan <yergor@mail.ru>
2020-02-03 19:30:29 +0500

    name

--- web_project/profac/views.py
+++ web_project/profac/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from django.http import request
 from django.http import HttpResponseRedirect
 
+
 # Create your views here.
 def profile(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect('/login/?next=%s' % request.path)
     else:
         return render(request, 'profac/profile.html')
-    
+
+def get_full_name(request):
+    return {'request': request}
 
 
 # Create your views here.
