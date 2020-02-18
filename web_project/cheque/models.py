67bb0d2a66dec9866cc703aa06c52841f03b34e1
g.ericyan <yergor@mail.ru>
2020-02-18 18:46:10 +0500

    Чек

--- web_project/cheque/models.py
+++ web_project/cheque/models.py
@@ -7,10 +7,10 @@ from django.utils import timezone
 class Post(models.Model):
     def default_datetime(): return timezone.now()
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    chequeAmount = models.CharField(max_length=200, help_text='Сумма чека*', verbose_name='На какую сумму у вас есть чек?')
-    chequeDate = models.CharField(max_length=100, help_text='Дата совершения покупки',)
-    chequeThing = models.CharField(max_length=200, help_text='Товар*', verbose_name='Вещи которые вы купили?')
-    shop = models.IntegerField(default=1, verbose_name='Магазин', help_text='Откуда вы купили товар?',) 
+    chequeAmount = models.CharField(max_length=200, help_text='Сумма чека', verbose_name='На какую сумму у вас есть чек?')
+    chequeDate = models.CharField(max_length=100, help_text='Дата покупки указанная в чеке',verbose_name='Дата совершения покупки')
+    chequeThing = models.CharField(max_length=200, help_text='Товар', verbose_name='Вещи которые вы купили?')
+    shop = models.CharField(max_length=200, verbose_name='Магазин', help_text='Откуда вы купили товар?',) 
     date = models.DateTimeField(default=default_datetime, verbose_name='Дата добавления чека')
-    ref = models.CharField(max_length=200,default=default_datetime, verbose_name='Ссылка на покупателя')
+    ref = models.CharField(max_length=200, verbose_name='Ссылка на покупателя')
 
